7b9208ed8668e4b27257305c469241ba821b5b5e
Simon Bjørnøy <simon.bjornoy@gmail.com>
2024-09-03 09:54:42 +0200

    line-number

--- system/set-iterm-font.sh
+++ system/set-iterm-font.sh
@@ -3,7 +3,7 @@
 #
 
 FILE=$(find /Applications/iTerm.app/ -name DefaultBookmark.plist)
-LINE=$(grep -A1 "Normal Font" $FILE)
+LINE=$(grep -A1 "Normal Font" -n $FILE | grep "string" | awk '{print $1}' FS="-")
 
 if (echo $LINE); then
   sed -i '' -e "${LINE}s/Monaco 12/JetBrainsMono Nerd Mono Font 14/" $FILE
