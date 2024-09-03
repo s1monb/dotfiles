ace33a918eff57d265f80dfb187d940bba9cb288
Simon Bjørnøy <simon.bjornoy@gmail.com>
2024-09-03 09:49:20 +0200

    add iterm font-stuff

---
+++ system/set-iterm-font.sh
@@ -0,0 +1,10 @@
+#!/bin/usr/env bash
+# Configures iterm default font
+#
+
+FILE=$(find /Applications/iTerm.app/ -name DefaultBookmark.plist)
+LINE=$(grep -A1 "Normal Font" $FILE)
+
+if (echo $LINE); then
+  sed -i '' -e "${LINE}s/Monaco 12/JetBrainsMono Nerd Mono Font 14/" $FILE
+fi
